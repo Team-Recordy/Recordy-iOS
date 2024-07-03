fb48b827ccc60c3d9c5d756b72dfe71f9b6c2a79
sozohoy <sozohoy@gmail.com>
2024-07-04 03:07:52 +0900

    feat: 저장 영상 불러오기, 영상 선택 기능 구현(#10)

--- Core/Sources/Photos/PhotoKitManager.swift
+++ Core/Sources/Photos/PhotoKitManager.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2024 com.recordy. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Photos
+
+public final class PhotoKitManager {
+  static public func getAssetThumbnail(
+    asset: PHAsset,
+    size: CGSize
+  ) -> UIImage? {
+    let manager = PHImageManager.default()
+    let options = PHImageRequestOptions()
+    options.isSynchronous = true
+    options.isNetworkAccessAllowed = true
+    options.deliveryMode = .highQualityFormat
+    
+    var thumbnail: UIImage?
+    manager.requestImage(
+      for: asset,
+      targetSize: size,
+      contentMode: .aspectFill,
+      options: options
+    ) { (image, info) in
+      guard let thumbnailUnwrapped = image else {return}
+      thumbnail = thumbnailUnwrapped
+    }
+    return thumbnail
+  }
+}
