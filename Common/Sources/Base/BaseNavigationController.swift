daf0192661cc8bbf5f46e9641f9d79988133f655
sozohoy <sozohoy@gmail.com>
2024-07-04 03:03:57 +0900

    feat: BaseNavigationController(#10)

--- Common/Sources/Base/BaseNavigationController.swift
+++ Common/Sources/Base/BaseNavigationController.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2024 com.recordy. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public class BaseNavigationController: UINavigationController {
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+    configureNavigationBar()
+  }
+
+  private func configureNavigationBar() {
+    let appearance = UINavigationBarAppearance()
+    appearance.backgroundColor = .black
+    appearance.shadowColor = UIColor.clear
+    navigationBar.standardAppearance = appearance
+    navigationBar.scrollEdgeAppearance = appearance
+    navigationBar.titleTextAttributes = [
+      NSAttributedString.Key.foregroundColor: CommonAsset.recordyWhite,
+      NSAttributedString.Key.font: RecordyFont.title2
+    ]
+    navigationItem.backButtonDisplayMode = .minimal
+  }
+}
