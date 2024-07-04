00a2a7a365b6889565307abae20abacf99fe9a56
Chandrala <gudrms2538@gmail.com>
2024-07-04 22:32:58 +0900

    feat: UITextField+ 구현

--- Common/Sources/Extension/UITextField+.swift
+++ Common/Sources/Extension/UITextField+.swift
@@ -9,4 +9,36 @@
 import UIKit
 
 extension UITextField {
+  func setPlaceholder(
+    placeholder: String,
+    placeholderColor: UIColor,
+    font: UIFont
+  ) {
+    attributedPlaceholder = NSAttributedString(
+      string: placeholder,
+      attributes: [.foregroundColor: placeholderColor, .font: font]
+    )
+    self.font = font
+  }
+  
+  func setLayer(
+    borderColor: UIColor = .clear,
+    borderWidth: CGFloat = 0,
+    cornerRadius: CGFloat = 0
+  ){
+    layer.borderColor = borderColor.cgColor
+    layer.borderWidth = borderWidth
+    layer.cornerRadius = cornerRadius
+  }
+  
+  func addPadding(left: CGFloat? = nil, right: CGFloat? = nil) {
+    if let left {
+      leftView = UIView(frame: CGRect(x: 0, y: 0, width: left, height: 0))
+      leftViewMode = .always
+    }
+    if let right {
+      rightView = UIView(frame: CGRect(x: 0, y: 0, width: right, height: 0))
+      rightViewMode = .always
+    }
+  }
 }
