bfacc1845dc258a9558e717418225cc010ffbeab
Chandrala <gudrms2538@gmail.com>
2024-07-04 22:26:09 +0900

    add: RecordyTextField, UITextField+ 파일 추가(#12)

---
+++ Common/Sources/Extension/UITextField+.swift
@@ -0,0 +1,12 @@
+//
+//  UITextField+.swift
+//  Common
+//
+//  Created by Chandrala on 7/4/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+}
---
+++ Common/Sources/RecordyTextField.swift
@@ -0,0 +1,20 @@
+//
+//  RecordyTextField.swift
+//  Common
+//
+//  Created by Chandrala on 7/4/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import UIKit
+
+final class RecordyTextField: UITextField {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
