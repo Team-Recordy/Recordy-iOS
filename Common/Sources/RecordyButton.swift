4b9e5d5ddc71b70dcd6864d986b89fbffe106b71
0gonge <Challengef0802@gmail.com>
2024-07-08 04:56:44 +0900

    feat: RecordyButton component 구현(#13)

---
+++ Common/Sources/RecordyButton.swift
@@ -0,0 +1,52 @@
+//
+//  RecordyButton.swift
+//  Common
+//
+//  Created by 송여경 on 7/5/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+enum ButtonState {
+  case active
+  case deactive
+}
+
+class RecordyButton: UIButton {
+  
+  var buttonState: ButtonState = .deactive {
+    didSet {
+      updateButtonAppearance()
+    }
+  }
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func setUI() {
+    layer.cornerRadius = 12
+    titleLabel?.font = RecordyFont.button1.font
+    updateButtonAppearance()
+  }
+  
+  private func updateButtonAppearance() {
+    switch buttonState {
+    case .active:
+      backgroundColor = CommonAsset.recordyMain.color
+      setTitleColor(CommonAsset.recordyGrey09.color, for: .normal)
+    case .deactive:
+      backgroundColor = CommonAsset.recordyGrey08.color
+      setTitleColor(CommonAsset.recordyGrey04.color, for: .normal)
+    }
+  }
+}
+
