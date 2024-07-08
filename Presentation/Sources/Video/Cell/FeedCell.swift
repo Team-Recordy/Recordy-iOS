932e5a29744ac6962a98bc90944d13d3de5fae1c
sozohoy <sozohoy@gmail.com>
2024-07-08 21:06:00 +0900

    feat: FeedCell 영상 출력 구현(#16)

--- Presentation/Sources/Video/Cell/FeedCell.swift
+++ Presentation/Sources/Video/Cell/FeedCell.swift
@@ -7,7 +7,53 @@
 //
 
 import UIKit
+import AVKit
+
+import SnapKit
+import Then
 
 class FeedCell: UICollectionViewCell {
-    
+
+  var avQueuePlayer: AVQueuePlayer?
+  var avPlayerLayer: AVPlayerLayer?
+
+  private let playerView = UIView()
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setUI()
+    setAutolayout()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func setUI() {
+    self.addSubview(playerView)
+  }
+
+  private func setAutolayout() {
+    self.playerView.snp.makeConstraints {
+      $0.verticalEdges.horizontalEdges.equalToSuperview()
+    }
+  }
+
+  private func addPlayer(
+    for url: URL,
+    bounds: CGRect
+  ) {
+    avQueuePlayer = AVQueuePlayer(url: url)
+    avPlayerLayer = AVPlayerLayer(player: self.avQueuePlayer!)
+    avPlayerLayer?.frame = bounds
+    avPlayerLayer?.fillMode = .both
+    avPlayerLayer?.videoGravity = .resizeAspectFill
+    playerView.layer.addSublayer(self.avPlayerLayer!)
+  }
+
+  func bind(url: URL?, bounds: CGRect) {
+    if let url = url {
+      addPlayer(for: url, bounds: bounds)
+    }
+  }
 }
