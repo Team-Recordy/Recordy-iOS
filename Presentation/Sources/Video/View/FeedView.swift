418c68c8a9f7856563a7d6a9aaf1d78ef6076e81
sozohoy <sozohoy@gmail.com>
2024-07-08 21:05:44 +0900

    add: FeedView(#16)

---
+++ Presentation/Sources/Video/View/FeedView.swift
@@ -0,0 +1,21 @@
+//
+//  FeedView.swift
+//  Presentation
+//
+//  Created by 한지석 on 7/8/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import UIKit
+
+class FeedView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
