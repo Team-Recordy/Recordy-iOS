f425b1a67001041769b1de414055b9b4d237a5eb
0gonge <Challengef0802@gmail.com>
2024-07-09 21:57:21 +0900

    fix: codeReview보완(#25)

---
+++ Common/Sources/RecordyToastMessageView.swift
@@ -0,0 +1,13 @@
+//
+//  RecordyToastMessageView.swift
+//  Common
+//
+//  Created by 송여경 on 7/9/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import UIKit
+
+private let toastImage = UIImageView().then {
+  
+}
