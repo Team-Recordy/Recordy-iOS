e12754e1d4e48646d9788ca1249bf4d7e05dcc41
Chandrala <gudrms2538@gmail.com>
2024-07-09 22:36:01 +0900

    feat: RecordyProgressView 구현(#24)

--- Common/Sources/RecordyProgressView.swift
+++ Common/Sources/RecordyProgressView.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-final class RecordyProgressView: UIProgressView {
+final class RecordyProgressView: UIView {
   
-  init() {
-    super.init(frame: .zero)
+  var ratio: CGFloat = 0.0 {
+    didSet {
+      self.isHidden = !self.ratio.isLess(than: 1.0)
+      
+      self.progressBarView.snp.remakeConstraints {
+        $0.top.bottom.equalToSuperview()
+        $0.width.equalToSuperview().multipliedBy(self.ratio)
+      }
+      
+      UIView.animate(
+        withDuration: 0.5,
+        delay: 0,
+        options: .curveEaseInOut,
+        animations: self.layoutIfNeeded,
+        completion: nil
+      )
+    }
+  }
+  
+  let progressBarView = UIView().then {
+    $0.backgroundColor = CommonAsset.recordyMain.color
+  }
+  
+  init(frame: CGRect = .zero, ratio: CGFloat = 0.0) {
+    self.ratio = ratio
+    super.init(frame: frame)
+    setStyle()
+    setUI()
+    setAutoLayout()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+  
+  func setStyle() {
+    self.isUserInteractionEnabled = false
+    self.backgroundColor = CommonAsset.recordySub01.color
+  }
+  
+  func setUI() {
+    self.addSubview(progressBarView)
+  }
+  
+  func setAutoLayout() {
+    progressBarView.snp.makeConstraints {
+      $0.top.bottom.equalToSuperview()
+      $0.width.equalToSuperview().multipliedBy(ratio)
+    }
   }
 }
