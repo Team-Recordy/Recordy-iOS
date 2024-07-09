6f87090bbc095e4faff4b7b404a015596bf83af0
0gonge <Challengef0802@gmail.com>
2024-07-09 17:24:40 +0900

    feat:chip_keyword 구현(#20)

---
+++ Common/Sources/ChipKeyWordButton.swift
@@ -0,0 +1,53 @@
+//
+//  ChipKeyWordButton.swift
+//  Common
+//
+//  Created by 송여경 on 7/9/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+enum ChipState {
+  case activate
+  case deactive
+}
+
+class ChipKeyWordButton: UIButton {
+  var chipstate: ChipState = .deactive{
+    didSet {
+      updateChipAppearance()
+    }
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setUI()
+  }
+  
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  private func setUI(){
+    layer.cornerRadius = 30
+    titleLabel?.font = RecordyFont.body2.font
+    updateChipAppearance()
+  }
+  
+  private func updateChipAppearance() {
+    switch chipstate {
+    case .activate:
+      backgroundColor = CommonAsset.recordyGrey08.color
+      setTitleColor(CommonAsset.recordyMain.color, for: .normal)
+      layer.borderColor = CommonAsset.recordyMain.color.cgColor
+      layer.borderWidth = 1
+
+    case .deactive:
+      backgroundColor = CommonAsset.recordyGrey08.color
+      setTitleColor(CommonAsset.recordyGrey04.color, for: .normal)
+    }
+  }
+}
--- Common/Sources/ChipKeyWorldButton.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ChipKeyWorldButton.swift
-//  Common
-//
-//  Created by 송여경 on 7/9/24.
-//  Copyright © 2024 com.recordy. All rights reserved.
-//
-
-import Foundation
