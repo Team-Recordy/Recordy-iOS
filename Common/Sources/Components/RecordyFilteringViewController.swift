ad58805e5dd4d419888676f2fab217f473a56b88
sozohoy <sozohoy@gmail.com>
2024-07-11 06:26:11 +0900

    add: 필요 파일 추가(#38)

---
+++ Common/Sources/Components/RecordyFilteringViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecordyFilteringViewController.swift
+//  Common
+//
+//  Created by 한지석 on 7/10/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import UIKit
+
+class RecordyFilteringViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
