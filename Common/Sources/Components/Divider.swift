d525a2af2e115a14039018230071d50ce5e72481
sozohoy <sozohoy@gmail.com>
2024-07-10 19:18:58 +0900

    style: 불필요 공백 제거(#36)

--- Common/Sources/Components/Divider.swift
+++ Common/Sources/Components/Divider.swift
@@ -13,11 +13,9 @@ public final class Divider: UIView {
   public init(color: UIColor) {
     super.init(frame: .zero)
     self.backgroundColor = color
-    
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
-    
   }
 }
