59009f4ee01068aeba3b2b0d85652e96ee9cb777
sozohoy <sozohoy@gmail.com>
2024-07-11 06:29:00 +0900

    feat: CollectionView 왼쪽 정렬 적용 Layout(#38)

--- Common/Sources/Components/LeftAlignedCollectionViewFlowLayout.swift
+++ Common/Sources/Components/LeftAlignedCollectionViewFlowLayout.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2024 com.recordy. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LeftAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout {
+
+  override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+    let attributes = super.layoutAttributesForElements(in: rect)
+
+    var leftMargin = sectionInset.left
+    var maxY: CGFloat = -1.0
+    attributes?.forEach { layoutAttribute in
+      if layoutAttribute.frame.origin.y >= maxY {
+        leftMargin = sectionInset.left
+      }
+
+      layoutAttribute.frame.origin.x = leftMargin
+      leftMargin += layoutAttribute.frame.width + minimumInteritemSpacing
+      maxY = max(layoutAttribute.frame.maxY , maxY)
+    }
+
+    return attributes
+  }
+}
