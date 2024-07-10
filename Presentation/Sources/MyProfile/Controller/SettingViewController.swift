df86b839fd26f69703bab450280a2e33653d4855
0gonge <Challengef0802@gmail.com>
2024-07-11 07:04:38 +0900

    Feat: footerview alignment 조절, 클릭 시 회색 제거(#41)

--- Presentation/Sources/MyProfile/Controller/SettingViewController.swift
+++ Presentation/Sources/MyProfile/Controller/SettingViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Common
+import Then
 
 public class SettingViewController: UIViewController {
   
@@ -30,32 +31,29 @@ public class SettingViewController: UIViewController {
   public override func viewDidLoad() {
     super.viewDidLoad()
     
-    tableView = UITableView(frame: view.bounds, style: .grouped)
-    tableView.delegate = self
-    tableView.dataSource = self
-    
-    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    tableView = UITableView(frame: view.bounds, style: .grouped).then {
+      $0.delegate = self
+      $0.dataSource = self
+      $0.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+      $0.backgroundColor = .black
+    }
     
     view.backgroundColor = .black
-    tableView.backgroundColor = .black
-    
-    let header = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 64))
-    let footer = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 8))
-    
-    header.backgroundColor = .red
-    footer.backgroundColor = CommonAsset.recordyGrey09.color
-    
-    let headerLabel = UILabel(frame: header.bounds)
-    headerLabel.text = "도움말"
-    headerLabel.textAlignment = .left
-    headerLabel.font = RecordyFont.title3.font
-    headerLabel.textColor = CommonAsset.recordyGrey01.color
-    header.addSubview(headerLabel)
-    
-    tableView.tableHeaderView = header
-    tableView.tableFooterView = footer
-    
     view.addSubview(tableView)
+    
+    let footerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 18)).then {
+      let versionLabel = UILabel(frame: CGRect(x: 20, y: 0, width: $0.frame.width - 20, height: $0.frame.height)).then {
+        $0.text = "앱 버전 1.1.1"
+        $0.font = RecordyFont.caption2.font
+        $0.textColor = CommonAsset.recordyGrey04.color
+        $0.textAlignment = .left
+      }
+      $0.addSubview(versionLabel)
+    }
+    tableView.do {
+      $0.tableFooterView = footerView
+    }
+
   }
 }
 
@@ -70,16 +68,28 @@ extension SettingViewController: UITableViewDataSource {
     return 64.0
   }
   
-  public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-    return section == 0 ? "도움말" : "기타"
+  public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    return UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 64)).then {
+      $0.backgroundColor = .black
+      let headerLabel = UILabel(frame: CGRect(x: 16, y: 0, width: $0.frame.width - 32, height: 64)).then {
+        $0.font = RecordyFont.title3.font
+        $0.textColor = CommonAsset.recordyGrey01.color
+        $0.textAlignment = .left
+        $0.text = section == 0 ? "도움말" : "기타"
+      }
+      $0.addSubview(headerLabel)
+    }
   }
   
   public func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-    return 8.0
+    return section == 0 ? 8.0 : 0.0
   }
   
-  public func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-    return nil
+  public func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+    guard section == 0 else { return nil }
+    return UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 8)).then {
+      $0.backgroundColor = CommonAsset.recordyGrey09.color
+    }
   }
   
   public func numberOfSections(in tableView: UITableView) -> Int {
@@ -91,11 +101,16 @@ extension SettingViewController: UITableViewDataSource {
   }
   
   public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-    cell.textLabel?.text = data[indexPath.section][indexPath.row]
-    cell.textLabel?.font = RecordyFont.body1.font
-    cell.textLabel?.textColor = CommonAsset.recordyGrey01.color
-    cell.backgroundColor = .black
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath).then {
+      $0.textLabel?.text = data[indexPath.section][indexPath.row]
+      $0.textLabel?.font = RecordyFont.body1.font
+      $0.textLabel?.textColor = CommonAsset.recordyGrey01.color
+      $0.backgroundColor = .black
+      $0.selectionStyle = .none 
+      
+      let arrowImageView = UIImageView(image: CommonAsset.indicator.image)
+      $0.accessoryView = arrowImageView
+    }
     return cell
   }
 }
