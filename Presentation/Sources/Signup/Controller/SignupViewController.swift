390bfdb7b94891e92d492dd00aea3debe93e3508
Chandrala <gudrms2538@gmail.com>
2024-07-11 16:19:08 +0900

    chore: Signup 관련 파일 추가(#44)

---
+++ Presentation/Sources/Signup/Controller/SignupViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SignupViewController.swift
+//  Presentation
+//
+//  Created by Chandrala on 7/11/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import UIKit
+
+final class SignupViewController: UIViewController {
+  
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+    setStyle()
+    setUI()
+    setAutoLayout()
+  }
+  
+  func setStyle() {
+    
+  }
+  
+  func setUI() {
+    
+  }
+  
+  func setAutoLayout() {
+    
+  }
+}
---
+++ Presentation/Sources/Signup/View/CompleteView.swift
@@ -0,0 +1,9 @@
+//
+//  CompleteView.swift
+//  Presentation
+//
+//  Created by Chandrala on 7/11/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import Foundation
