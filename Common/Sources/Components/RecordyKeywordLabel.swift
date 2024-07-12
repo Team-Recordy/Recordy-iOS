5bb21c29a5b4c42ffc73f9c33e8036aecc2e6e06
sozohoy <sozohoy@gmail.com>
2024-07-12 09:23:06 +0900

    add: 파일 추가(#47)

---
+++ Common/Sources/Components/RecordyKeywordLabel.swift
@@ -0,0 +1,21 @@
+//
+//  RecordyKeywordLabel.swift
+//  Common
+//
+//  Created by 한지석 on 7/12/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import UIKit
+
+class RecordyKeywordLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Common/Sources/Components/RecordySubtitleLabel.swift
@@ -0,0 +1,21 @@
+//
+//  RecordySubtitleLabel.swift
+//  Common
+//
+//  Created by 한지석 on 7/12/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import UIKit
+
+class RecordySubtitleLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
