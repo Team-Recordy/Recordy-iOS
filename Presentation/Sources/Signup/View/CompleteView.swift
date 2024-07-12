b08b3bc0a41b2effc83d8a7bbcca5808898f6a2f
Chandrala <gudrms2538@gmail.com>
2024-07-12 14:23:57 +0900

    feat: NickNameView, CompleteView 구현(#44)

--- Presentation/Sources/Signup/View/CompleteView.swift
+++ Presentation/Sources/Signup/View/CompleteView.swift
@@ -6,11 +6,19 @@
 //  Copyright © 2024 com.recordy. All rights reserved.
 //
 
-import Foundation
-
 import UIKit
 
+import SnapKit
+import Then
+
+import Common
+
 final class CompleteView: UIView {
+  
+  let completeImage = UIImageView()
+  let completeText1 = UILabel()
+  let completeText2 = UILabel()
+  let completeButton = RecordyButton()
   
   public override init(frame: CGRect) {
     super.init(frame: frame)
@@ -24,12 +32,59 @@ final class CompleteView: UIView {
   }
   
   func setStyle() {
+    completeImage.do {
+      $0.image = CommonAsset.signupComplete.image
+    }
+    
+    completeText1.do {
+      $0.text = "회원가입이 완료되었어요!"
+      $0.font = RecordyFont.title1.font
+      $0.textColor = CommonAsset.recordyGrey01.color
+    }
+    
+    completeText2.do {
+      $0.text = "지금 영상을 둘러보고 나만의 공간 취향을 발견해 보세요"
+      $0.font = RecordyFont.body2.font
+      $0.textColor = CommonAsset.recordyGrey03.color
+    }
+    
+    completeButton.do {
+      $0.setTitle("다음", for: .normal)
+    }
   }
   
   func setUI() {
+    self.addSubviews(
+      completeImage,
+      completeText1,
+      completeText2,
+      completeButton
+    )
   }
   
   func setAutoLayout() {
+    completeImage.snp.makeConstraints {
+      $0.top.equalToSuperview().offset(294)
+      $0.width.equalTo(100.adaptiveWidth)
+      $0.height.equalTo(100.adaptiveHeight)
+      $0.centerX.equalToSuperview()
+    }
+    
+    completeText1.snp.makeConstraints {
+      $0.top.equalTo(completeImage.snp.bottom).offset(16)
+      $0.centerX.equalToSuperview()
+    }
+    
+    completeText2.snp.makeConstraints {
+      $0.top.equalTo(completeText1.snp.bottom).offset(10)
+      $0.centerX.equalToSuperview()
+    }
+    
+    completeButton.snp.makeConstraints {
+      $0.horizontalEdges.equalToSuperview().inset(20)
+      $0.bottom.equalTo(safeAreaLayoutGuide).inset(14)
+      $0.height.equalTo(54.adaptiveHeight)
+    }
   }
 }
 
