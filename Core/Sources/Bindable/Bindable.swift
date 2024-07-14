35f248bfbbed5af54723cbede0ea4915ded55ff8
0gonge <Challengef0802@gmail.com>
2024-07-15 00:46:22 +0900

    feat: FollowingView 구현(#58)

--- Core/Sources/Bindable/Bindable.swift
+++ Core/Sources/Bindable/Bindable.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+public class Bindable<T> {
+  public var value: T {
+    didSet {
+      observer?(value)
+    }
+  }
+
+  public var observer: ((T) -> Void)?
+
+  public init(_ value: T) {
+    self.value = value
+  }
+
+  public func bind(_ observer: @escaping (T) -> Void) {
+    self.observer = observer
+    observer(value)
+  }
+}
