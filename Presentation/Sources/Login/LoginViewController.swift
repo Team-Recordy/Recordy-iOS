0d641807cd57fa0785c6e9c84fd7ffed508f7dfe
sozohoy <sozohoy@gmail.com>
2024-07-15 18:56:07 +0900

    feat: Login API 연결 - 카카오, 애플(#68)

--- Presentation/Sources/Login/LoginViewController.swift
+++ Presentation/Sources/Login/LoginViewController.swift
@@ -7,31 +7,132 @@
 //
 
 import UIKit
+import AuthenticationServices
 
-final class LoginViewController: UIViewController {
-  
+import Core
+
+import RxSwift
+import RxCocoa
+
+public final class LoginViewController: UIViewController {
+
   var rootView = LoginView()
-  
-  override func loadView() {
+  private var appleLoginCompletion: ((Result<String, Error>) -> Void)?
+  private let disposeBag = DisposeBag()
+
+  public override func loadView() {
     self.view = rootView
   }
-  
+
   public override func viewDidLoad() {
     super.viewDidLoad()
+    setTarget()
   }
-  
+
   func setTarget() {
-    rootView.kakaoLoginButton.addTarget(self, action: #selector(kakaoLoginButtonTapped), for: .touchUpInside)
-    rootView.appleLoginButton.addTarget(self, action: #selector(appleLoginButtonTapped), for: .touchUpInside)
+    rootView.kakaoLoginButton.addTarget(
+      self,
+      action: #selector(kakaoLoginButtonTapped),
+      for: .touchUpInside
+    )
+    rootView.appleLoginButton.addTarget(
+      self,
+      action: #selector(appleLoginButtonTapped),
+      for: .touchUpInside
+    )
   }
-  
+
   @objc
-  private func kakaoLoginButtonTapped(_ sender: UIButton) {
+  private func kakaoLoginButtonTapped() {
+    let kakaoLoginManager = KakaoLoginManager()
+    kakaoLoginManager.login { [weak self] result in
+      guard let self = self else { return }
+      switch result {
+      case .success(let accessToken):
+        self.postSignInRequest(
+          authorization: accessToken,
+          platformType: .kakao
+        )
+      case .failure(let failure):
+        print(failure)
+      }
+    }
   }
-  
+
   @objc
   private func appleLoginButtonTapped(_ sender: UIButton) {
+    loginWithApple { result in
+      switch result {
+      case .success(let accessToken):
+        self.postSignInRequest(
+          authorization: accessToken,
+          platformType: .apple
+        )
+      case .failure(let failure):
+        print(failure)
+      }
+    }
   }
-  
-  
+
+  func postSignInRequest(authorization: String, platformType: PlatformType) {
+    let apiProvider = APIProvider<APITarget.Users>()
+    let request = DTO.SignInRequest(authorization: authorization,
+                                    platformType: platformType)
+    apiProvider.request(DTO.SignInResponse.self, target: .signIn(request))
+      .subscribe(onSuccess: { result in
+        switch result {
+        case .success(let response):
+          print("@Log - \(response)")
+        case .failure(let error):
+          print("Sign in failed with error: \(error)")
+        }
+      }, onFailure: { error in
+        print("Sign in request failed with error: \(error)")
+      })
+      .disposed(by: disposeBag)
+  }
+}
+
+extension LoginViewController: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+
+  func loginWithApple(completion: @escaping (Result<String, Error>) -> Void) {
+    let appleIDProvider = ASAuthorizationAppleIDProvider()
+    let request = appleIDProvider.createRequest()
+    request.requestedScopes = [.fullName, .email]
+
+    let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+    authorizationController.delegate = self
+    authorizationController.presentationContextProvider = self
+    authorizationController.performRequests()
+    self.appleLoginCompletion = completion
+  }
+
+  public func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+    return self.view.window!
+  }
+
+  public func authorizationController(
+    controller: ASAuthorizationController,
+    didCompleteWithAuthorization authorization: ASAuthorization
+  ) {
+    if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+      if let authorizationCode = appleIDCredential.authorizationCode,
+         let identityToken = appleIDCredential.identityToken,
+         let authCodeString = String(data: authorizationCode, encoding: .utf8),
+         let identifyTokenString = String(data: identityToken, encoding: .utf8) {
+        appleLoginCompletion?(.success(identifyTokenString))
+      }
+    } else {
+      appleLoginCompletion?(.failure(RecordyNetworkError.apple))
+    }
+  }
+
+
+  public func authorizationController(
+    controller: ASAuthorizationController,
+    didCompleteWithError error: Error
+  ) {
+    // 로그인 실패(유저의 취소도 포함)
+    print("login failed - \(error.localizedDescription)")
+  }
 }
