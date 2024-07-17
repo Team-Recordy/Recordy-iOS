08d8d9506768e3bf9e4a70e24d6869c80f7d0697
sozohoy <sozohoy@gmail.com>
2024-07-17 21:32:17 +0900

    add: 필요 파일 추가(#86)

---
+++ Core/Sources/Network/DTO/Request/Bookmark/DTO+PostBookmarkRequest.swift
@@ -0,0 +1,9 @@
+//
+//  DTO+PostBookmarkRequest.swift
+//  Core
+//
+//  Created by 한지석 on 7/17/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import Foundation
---
+++ Presentation/Sources/Video/UserProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserProfileViewController.swift
+//  Presentation
+//
+//  Created by 한지석 on 7/17/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
