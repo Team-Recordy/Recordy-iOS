ec0750860157adfb076234460d917f47ede04ab4
0gonge <Challengef0802@gmail.com>
2024-07-18 12:57:31 +0900

    feat: popupview 구현(#104)

--- Common/Sources/Components/RecordyPopUpType.swift
+++ Common/Sources/Components/RecordyPopUpType.swift
@@ -6,4 +6,104 @@
 //  Copyright © 2024 com.recordy. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public enum RecordyPopUpType {
+  case permission
+  case exit
+  case signOut
+  case withdraw
+  case delete
+  
+  public var image: UIImage {
+    switch self {
+    case .permission:
+      return CommonAsset.permission.image
+    case .exit:
+      return CommonAsset.exit.image
+    case .signOut:
+      return CommonAsset.alert.image
+    case .withdraw:
+      return CommonAsset.alert.image
+    case .delete:
+      return CommonAsset.alert.image
+    }
+  }
+  
+  public var backgroundColor: UIColor {
+    return CommonAsset.recordyGrey08.color
+  }
+  
+  public var title: String {
+    switch self {
+    case .permission:
+      "필수 권한을 허용해주세요"
+    case .exit:
+      "화면을 나가시겠어요?"
+    case .signOut:
+      "로그아웃 하시겠어요?"
+    case .withdraw:
+      "정말 탈퇴하시겠어요?"
+    case .delete:
+      "삭제하시겠어요?"
+    }
+  }
+  
+  public var titleFont: UIFont {
+    return RecordyFont.title3.font
+  }
+  
+  public var subtitle: String {
+    switch self {
+    case .permission:
+      "사진 접근을 허용하여 영상을 업로드 하세요."
+    case .exit:
+      "지금까지 작성하신 내용이 모두 사라져요."
+    case .signOut:
+      "버튼을 누르면 로그인 페이지로 이동합니다."
+    case .withdraw:
+      "소중한 기록들이 모두 사라져요."
+    case .delete:
+      "해당 기록은 영구 삭제되며, 복구가 불가능해요"
+    }
+  }
+  
+  public var subtitleFont: UIFont {
+    return RecordyFont.caption1.font
+  }
+  
+  public var buttonTitle: String {
+    switch self {
+    case .permission:
+      "지금 설정"
+    case .exit:
+      "나가기"
+    case .signOut:
+      "로그아웃"
+    case .withdraw:
+      "탈퇴"
+    case .delete:
+      "삭제"
+    }
+  }
+  
+  public var buttonBackgroundColor: UIColor {
+    return CommonAsset.recordyMain.color
+  }
+  
+  public var buttonTitleColor: UIColor {
+    return CommonAsset.recordyGrey09.color
+  }
+  
+  public var closeButtonBackgroundColor: UIColor {
+    return CommonAsset.recordyGrey06.color
+  }
+  
+  public var closeButtonTitleColor: UIColor {
+    return CommonAsset.recordyGrey01.color
+  }
+  
+  public var buttonFont: UIFont {
+    return RecordyFont.button2.font
+  }
+}
---
+++ Common/Sources/Components/RecordyPopUpView.swift
@@ -0,0 +1,122 @@
+//
+//  RecordyPopUpView.swift
+//  Common
+//
+//  Created by 송여경 on 7/18/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+
+public class RecordyPopUpView: UIView {
+  public let popUpType: RecordyPopUpType
+  private let image = UIImageView()
+  private let titleLabel = UILabel()
+  private let subtitleLabel = UILabel()
+  private let leftButton = UIButton()
+  public let rightButton = UIButton()
+  
+  public init(type: RecordyPopUpType) {
+    self.popUpType = type
+    super.init(frame: .zero)
+    self.backgroundColor = CommonAsset.recordyGrey08.color
+    self.layer.cornerRadius = 20
+    setStyle()
+    setUI()
+    setAutoLayout()
+  }
+  
+  private func setStyle() {
+    image.do {
+      $0.image = popUpType.image
+    }
+    
+    titleLabel.do {
+      $0.text = popUpType.title
+      $0.font = popUpType.titleFont
+      $0.textAlignment = .center
+      $0.textColor = CommonAsset.recordyGrey01.color
+    }
+    
+    subtitleLabel.do {
+      $0.text = popUpType.subtitle
+      $0.font = popUpType.subtitleFont
+      $0.textAlignment = .center
+      $0.textColor = CommonAsset.recordyGrey01.color
+    }
+    
+    leftButton.do {
+      $0.setTitle("취소", for: .normal)
+      $0.backgroundColor = popUpType.closeButtonBackgroundColor
+      $0.setTitleColor(popUpType.closeButtonTitleColor, for: .normal)
+      $0.titleLabel?.font = popUpType.buttonFont
+      $0.addTarget(self, action: #selector(leftButtonTapped), for: .touchUpInside)
+      $0.cornerRadius(8)
+    }
+    
+    rightButton.do {
+      $0.setTitle(popUpType.buttonTitle, for: .normal)
+      $0.backgroundColor = popUpType.buttonBackgroundColor
+      $0.setTitleColor(popUpType.buttonTitleColor, for: .normal)
+      $0.titleLabel?.font = popUpType.buttonFont
+      $0.cornerRadius(8)
+      
+      //      $0.addTarget(self, action: #selector(rightButtonTapped), for: .normal)
+    }
+    
+  }
+  
+  private func setUI() {
+    self.addSubviews(image, titleLabel, subtitleLabel, leftButton, rightButton)
+  }
+  
+  private func setAutoLayout() {
+    self.snp.makeConstraints {
+      $0.width.equalTo(298.adaptiveWidth)
+      $0.height.equalTo(252.adaptiveHeight)
+    }
+    
+    image.snp.makeConstraints {
+      $0.top.equalToSuperview().offset(30)
+      $0.centerX.equalToSuperview()
+      $0.leading.equalToSuperview().offset(113)
+      $0.width.height.equalTo(72)
+    }
+    
+    titleLabel.snp.makeConstraints {
+      $0.top.equalTo(image.snp.bottom).offset(16)
+      $0.centerX.equalToSuperview()
+    }
+    subtitleLabel.snp.makeConstraints {
+      $0.top.equalTo(titleLabel.snp.bottom).offset(6)
+      $0.centerX.equalToSuperview()
+    }
+    
+    leftButton.snp.makeConstraints {
+      $0.top.equalTo(subtitleLabel.snp.bottom).offset(18)
+      $0.leading.equalToSuperview().offset(20)
+      $0.trailing.equalToSuperview().inset(153)
+      $0.height.equalTo(44.adaptiveHeight)
+      $0.width.equalTo(125.adaptiveWidth)
+    }
+    
+    rightButton.snp.makeConstraints {
+      $0.top.equalTo(subtitleLabel.snp.bottom).offset(18)
+      $0.leading.equalTo(leftButton.snp.trailing).offset(8)
+      $0.height.equalTo(44.adaptiveHeight)
+      $0.width.equalTo(125.adaptiveWidth)
+    }
+    
+  }
+  
+  @objc public func leftButtonTapped() {
+    self.isHidden = true
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
