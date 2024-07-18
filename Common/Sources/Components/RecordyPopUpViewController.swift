4a0e67af0ee99193f7f319d8c46e0a67d3f1c815
sozohoy <sozohoy@gmail.com>
2024-07-19 07:57:52 +0900

    feat: PopUpViewController 구현, Toast 수정(#102)

--- Common/Sources/Components/RecordyPopUpViewController.swift
+++ Common/Sources/Components/RecordyPopUpViewController.swift
@@ -8,23 +8,65 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class RecordyPopUpViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  let popUpView: RecordyPopUpView
+  var rightButtonAction: (() -> Void)?
 
-        // Do any additional setup after loading the view.
+  public init(
+    type: RecordyPopUpType,
+    rightButtonAction: (() -> Void)?
+  ) {
+    self.popUpView = RecordyPopUpView(type: type)
+    self.rightButtonAction = rightButtonAction
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setStyle()
+    setUI()
+    setAutoLayout()
+  }
+
+  private func setStyle() {
+    self.popUpView.rightButton.addTarget(
+      self,
+      action: #selector(rightButtonTapped),
+      for: .touchUpInside
+    )
+    self.popUpView.leftButton.addTarget(
+      self,
+      action: #selector(leftButtonTapped),
+      for: .touchUpInside
+    )
+  }
+
+  private func setUI() {
+    view.backgroundColor = .black.withAlphaComponent(0.5)
+    self.view.addSubview(popUpView)
+  }
+
+  private func setAutoLayout() {
+    self.popUpView.snp.makeConstraints {
+      $0.width.equalTo(290.adaptiveWidth)
+      $0.height.equalTo(256.adaptiveHeight)
+      $0.center.equalToSuperview()
     }
-    
+  }
 
-    /*
-    // MARK: - Navigation
+  @objc func rightButtonTapped() {
+    rightButtonAction?()
+  }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  @objc func leftButtonTapped() {
+    self.dismiss(animated: false)
+  }
 }
