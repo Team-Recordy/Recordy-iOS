959a95c80c9e05414734ea99d9cc659fba50d9d3
sozohoy <sozohoy@gmail.com>
2024-08-07 19:39:37 +0900

    refactor: 불필요 코드 제거, 예외처리, 순서 변경, 컨벤션, 무한스크롤(#135)

--- Presentation/Sources/Video/VideoFeedViewModel.swift
+++ Presentation/Sources/Video/VideoFeedViewModel.swift
@@ -10,8 +10,6 @@ import Foundation
 
 import Core
 
-import RxSwift
-
 enum VideoFeedType {
   case all
   case following
@@ -33,7 +31,6 @@ class VideoFeedViewModel {
   var hasNext = true
   var pageNumber = 0
   let apiProvider = APIProvider<APITarget.Records>()
-  let disposeBag = DisposeBag()
   var isFetching = false
   var onFeedListUpdate: (() -> ())?
   var isBookmarked: (() -> ())?
@@ -57,6 +54,8 @@ class VideoFeedViewModel {
     switch type {
     case .all:
       getRecordList(toggle: toggle ?? false)
+    case .following:
+      getFollowRecordList()
     case .famous:
       getFamousRecordList()
     case .recent:
@@ -65,8 +64,6 @@ class VideoFeedViewModel {
       getUserProfileRecordList()
     case .bookmarked:
       getBookmarkedFeedList()
-    case .following:
-      getFollowRecordList()
     default: return
     }
   }
@@ -77,9 +74,7 @@ class VideoFeedViewModel {
     isFetching = true
     let request = DTO.GetRecordListRequest(size: 15)
     apiProvider.requestResponsable(
-      .getRecordList(
-        request
-      ),
+      .getRecordList(request),
       DTO.RecordList.self
     ) { result in
       self.isFetching = false
@@ -92,7 +87,36 @@ class VideoFeedViewModel {
         }
         self.onFeedListUpdate?()
       case .failure(let error):
-        print(error)
+        //TODO: 실패 시 예외처리 필요
+        self.feedList = []
+        self.onFeedListUpdate?()
+      }
+    }
+  }
+
+  func getFollowRecordList() {
+    guard !isFetching,
+          let cursorId = cursorId
+    else { return }
+    isFetching = true
+    let request = DTO.GetFollowingRecordListRequest(
+      cursorId: cursorId,
+      size: 15
+    )
+    apiProvider.requestResponsable(
+      .getFollowingRecordList(request),
+      DTO.GetFollowingRecordListResponse.self
+    ) { result in
+      self.isFetching = false
+      switch result {
+      case .success(let response):
+        self.feedList += response.feeds
+        self.cursorId = response.nextCursor
+        self.onFeedListUpdate?()
+      case .failure(let failure):
+        //TODO: 실패 시 예외처리 필요
+        self.feedList = []
+        self.onFeedListUpdate?()
       }
     }
   }
@@ -129,6 +153,7 @@ class VideoFeedViewModel {
         self.pageNumber += 1
         self.onFeedListUpdate?()
       case .failure(let failure):
+        //TODO: 실패 시 예외처리 필요
         print(failure)
       }
     }
@@ -170,6 +195,7 @@ class VideoFeedViewModel {
         self.cursorId = response.nextCursor
         self.onFeedListUpdate?()
       case .failure(let failure):
+        //TODO: 실패 시 예외처리 필요
         print(failure)
       }
     }
@@ -198,6 +224,7 @@ class VideoFeedViewModel {
           self.onFeedListUpdate?()
         }
       case .failure(let failure):
+        //TODO: 실패 시 예외처리 필요
         print(failure)
       }
     }
@@ -223,25 +250,7 @@ class VideoFeedViewModel {
           self.onFeedListUpdate?()
         }
       case .failure(let failure):
-        print(failure)
-      }
-    }
-  }
-
-  func getFollowRecordList() {
-    guard !isFetching
-    else { return }
-    let request = DTO.GetFollowingRecordListRequest(
-      cursorId: 0,
-      size: 100
-    )
-    apiProvider.requestResponsable(.getFollowingRecordList(request), DTO.GetFollowingRecordListResponse.self) { result in
-      self.isFetching = false
-      switch result {
-      case .success(let response):
-        self.feedList = response.feeds
-        self.onFeedListUpdate?()
-      case .failure(let failure):
+        //TODO: 실패 시 예외처리 필요
         print(failure)
       }
     }
@@ -290,7 +299,6 @@ class VideoFeedViewModel {
 
   func deleteFeed(_ index: Int) {
     let feed = self.feedList[index]
-    let deleteIndex = self.feedList.firstIndex { $0 == feed }
     let request = DTO.DeleteRecordRequest(record_id: feed.id)
     apiProvider.justRequest(.deleteRecord(request)) { result in
       switch result {
@@ -318,146 +326,3 @@ class VideoFeedViewModel {
     }
   }
 }
-
-//
-//  func fetchMoreVideos(completion: @escaping () -> Void) {
-//    let feeds: [Feed] = Feed.mockdata1
-//    let dispatchGroup = DispatchGroup()
-//    var cachedFeeds: [Feed] = []
-//
-//    for feed in feeds {
-//      dispatchGroup.enter()
-//      VideoCacheManager.shared.downloadAndCacheURL(url: feed.videoLink) { url in
-//        guard url != nil else {
-//          dispatchGroup.leave()
-//          return
-//        }
-//        cachedFeeds.append(feed)
-//        dispatchGroup.leave()
-//      }
-//    }
-//    dispatchGroup.notify(queue: .main) { [weak self] in
-//      guard let self = self else { return }
-//      let newFeeds = cachedFeeds.filter { newFeed in
-//        !self.feedList.contains(where: { $0.id == newFeed.id })
-//      }
-//      self.feedList.append(contentsOf: newFeeds)
-//      completion()
-//    }
-//  }
-//class VideoFeedViewModel {
-//
-//  struct Input {
-//    let fetchVideos = PublishRelay<Void>()
-//    let currentIndex = BehaviorRelay<Int?>(value: nil)
-//    let bookmarkTapped = PublishRelay<Int>()
-//    let playRequest = PublishRelay<Int>()
-//    let fetchMoreVideos = PublishRelay<Void>()
-//  }
-//
-//  struct Output {
-//    let feedList = BehaviorRelay<[Feed]>(value: [])
-//    let currentFeed = BehaviorRelay<Feed?>(value: nil)
-//  }
-//
-//  private let disposeBag = DisposeBag()
-//  let input = Input()
-//  let output = Output()
-//
-//  init() {
-//    self.bind()
-//  }
-//
-//  func bind() {
-//    self.input.fetchVideos
-//      .subscribe { [weak self] _ in
-//        guard let self = self else { return }
-//        self.getVideos()
-//      }.disposed(by: disposeBag)
-//
-//    self.input.currentIndex
-//      .subscribe { [weak self] index in
-//        guard let self = self,
-//              let index = index
-//        else { return }
-//        let currentFeed = self.output.feedList.value[index]
-//        self.output.currentFeed.accept(currentFeed)
-//      }.disposed(by: disposeBag)
-//
-//    self.input.bookmarkTapped
-//      .subscribe { [weak self] index in
-//        guard let self = self else { return }
-//        self.bookmarkButtonTapped(index)
-//      }.disposed(by: disposeBag)
-//
-//    self.input.fetchMoreVideos
-//      .subscribe { [weak self] _ in
-//        guard let self = self else { return }
-//        self.getMoreVideos()
-//      }
-//      .disposed(by: disposeBag)
-//
-//
-//    self.input.fetchVideos.accept(())
-//  }
-//
-//  func getVideos() {
-//    let feeds: [Feed] = Feed.mockdata
-//    let dispatchGroup = DispatchGroup()
-//    var cachedFeeds: [Feed] = []
-//
-//    for feed in feeds {
-//      dispatchGroup.enter()
-//      VideoCacheManager
-//        .shared
-//        .downloadAndCacheURL(url: feed.videoLink) { url in
-//          guard url != nil else {
-//            dispatchGroup.leave()
-//            return
-//          }
-//          cachedFeeds.append(feed)
-//          dispatchGroup.leave()
-//        }
-//    }
-//    dispatchGroup.notify(queue: .main) { [weak self] in
-//      guard let self = self else { return }
-//      let newFeeds = cachedFeeds.filter { newFeed in
-//        !self.output.feedList.value.contains(where: { $0.id == newFeed.id })
-//      }
-//      self.output.feedList.accept(newFeeds)
-//      self.input.currentIndex.accept(0)
-//    }
-//  }
-//
-//  func getMoreVideos() {
-//    print(#function)
-//    let feeds: [Feed] = Feed.mockdata1
-//    let dispatchGroup = DispatchGroup()
-//    var cachedFeeds: [Feed] = []
-//
-//    for feed in feeds {
-//      dispatchGroup.enter()
-//      VideoCacheManager
-//        .shared
-//        .downloadAndCacheURL(url: feed.videoLink) { url in
-//          guard url != nil else {
-//            dispatchGroup.leave()
-//            return
-//          }
-//          cachedFeeds.append(feed)
-//          dispatchGroup.leave()
-//        }
-//    }
-//    print(cachedFeeds)
-//    dispatchGroup.notify(queue: .main) { [weak self] in
-//      guard let self = self else { return }
-//      self.output.feedList.accept(cachedFeeds)
-//    }
-//  }
-//
-//  private func bookmarkButtonTapped(_ index: Int) {
-//    var feeds = self.output.feedList.value
-//    feeds[index].isBookmarked.toggle()
-//    self.output.feedList.accept(feeds)
-//  }
-//}
