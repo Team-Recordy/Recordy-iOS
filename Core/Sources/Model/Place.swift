82c13a4e6f187e5a7c4f5cab3c247f3dc477c1a4
sozohoy <sozohoy@gmail.com>
2024-09-30 02:10:43 +0900

    refactor: 데이터 모델링 재정의(#147)

---
+++ Core/Sources/Model/Place.swift
@@ -0,0 +1,15 @@
+//
+//  Location.swift
+//  Core
+//
+//  Created by 한지석 on 9/29/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import Foundation
+
+struct Location {
+  let location
+  let title: String
+
+}
