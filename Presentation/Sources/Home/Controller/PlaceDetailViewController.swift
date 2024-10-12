cc1da3cfc6c50122a676ac8cbc3d553e7851cddd
Chandrala <gudrms2538@gmail.com>
2024-10-13 02:03:18 +0900

    chore: 폴더링 변경, PlaceDetail뷰 파일 추가(#154)

---
+++ Presentation/Sources/Home/Controller/PlaceDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PlaceDetailViewController.swift
+//  Presentation
+//
+//  Created by Chandrala on 10/13/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+//
+//  OverviewViewController.swift
+//  Presentation
+//
+//  Created by Chandrala on 10/12/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import UIKit
+
+import Common
+import Core
+
+final class PlaceDetailViewController: UIViewController {
+  
+  let placeDetailView = PlaceDetailView()
+  
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
---
+++ Presentation/Sources/Home/View/PlaceDetailView.swift
@@ -0,0 +1,35 @@
+//
+//  PlaceDetailView.swift
+//  Presentation
+//
+//  Created by Chandrala on 10/13/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import UIKit
+
+final class PlaceDetailView: UIView() {
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    setStyle()
+    setUI()
+    setAutolayout()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func setStyle() {
+    
+  }
+  
+  func setUI() {
+    
+  }
+  
+  func setAutolayout() {
+    
+  }
+}
