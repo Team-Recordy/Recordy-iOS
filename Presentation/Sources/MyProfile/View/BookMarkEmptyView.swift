64bd1269f3d09b6b57ed5100f2a4d6d1f891c9a1
0gonge <Challengef0802@gmail.com>
2024-10-19 02:15:21 +0900

    feat: BookMarkEmptyView구현(#159)

--- Presentation/Sources/MyProfile/View/BookMarkEmptyView.swift
+++ Presentation/Sources/MyProfile/View/BookMarkEmptyView.swift
@@ -1,5 +1,5 @@
 //
-//  MyRecordEmptyView.swift
+//  BookMarkEmptyView.swift
 //  Presentation
 //
 //  Created by 송여경 on 10/19/24.
@@ -12,12 +12,10 @@ import Then
 
 import Common
 
-class MyRecordEmptyView: UIView {
+class BookMarkEmptyView: UIView {
   private let imageView = UIImageView()
-  private let textLabel = UILabel()
-  private let goRecordButton = ViskitYellowButton()
-  
-  var onRecordButtonTapped: (() -> Void)?
+  private let titleLabel = UILabel()
+  private let goAroundButton = ViskitYellowButton()
   
   override init(frame: CGRect) {
     super.init(frame: frame)
@@ -38,25 +36,24 @@ class MyRecordEmptyView: UIView {
       $0.contentMode = .scaleAspectFit
     }
     
-    textLabel.do {
-      $0.text = "직접 방문한 공간 영상을\n공유해 보세요!"
+    titleLabel.do {
+      $0.text = "북마크한 영상이 없어요.\n영상을 둘러보고 저장해 보세요!"
       $0.numberOfLines = 2
       $0.textAlignment = .center
       $0.textColor = CommonAsset.viskitGray02.color
       $0.font = ViskitFont.title2.font
     }
     
-    goRecordButton.do {
-      $0.setTitle("영상 업로드하기", for: .normal)
-      $0.addTarget(self, action: #selector(goRecordButtonTapped), for: .touchUpInside)
+    goAroundButton.do {
+      $0.setTitle("영상 둘러보기", for: .normal)
     }
   }
   
   private func setUI() {
     [
       imageView,
-      textLabel,
-      goRecordButton
+      titleLabel,
+      goAroundButton
     ].forEach{ addSubview($0)}
   }
   
@@ -68,21 +65,28 @@ class MyRecordEmptyView: UIView {
       $0.height.equalTo(97.adaptiveHeight)
     }
     
-    textLabel.snp.makeConstraints {
+    titleLabel.snp.makeConstraints {
       $0.top.equalTo(imageView.snp.bottom).offset(14)
       $0.centerX.equalToSuperview()
-      $0.leading.trailing.equalToSuperview().inset(94)
+      $0.leading.trailing.equalToSuperview().inset(65)
     }
     
-    goRecordButton.snp.makeConstraints {
-      $0.top.equalTo(textLabel.snp.bottom).offset(24)
+    goAroundButton.snp.makeConstraints {
+      $0.top.equalTo(titleLabel.snp.bottom).offset(24)
       $0.centerX.equalToSuperview()
-      $0.width.equalTo(125.adaptiveWidth)
+      $0.width.equalTo(113.adaptiveWidth)
       $0.height.equalTo(44.adaptiveHeight)
     }
   }
   
-  @objc private func goRecordButtonTapped() {
-    onRecordButtonTapped?()
+  func setActionButtonHandler(_ handler: @escaping () -> Void) {
+    goAroundButton.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside)
+    self.actionButtonHandler = handler
+  }
+  
+  private var actionButtonHandler: (() -> Void)?
+  
+  @objc private func actionButtonTapped() {
+    actionButtonHandler?()
   }
 }
