7687edf0927e03bef7de12622597379deb8d109c
sozohoy <sozohoy@gmail.com>
2024-10-21 03:34:08 +0900

    add: ReportViewController(#161)

---
+++ Presentation/Sources/Video/Report/ReportViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ReportViewController.swift
+//  Presentation
+//
+//  Created by 한지석 on 10/21/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import UIKit
+
+class ReportViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+}
