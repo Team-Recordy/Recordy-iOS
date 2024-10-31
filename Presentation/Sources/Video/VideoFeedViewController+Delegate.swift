13caec0680e7441588da4171dc1e1bdca403c13b
sozohoy <sozohoy@gmail.com>
2024-10-31 22:44:21 +0900

    feat: VideoFeedVC 델리게이트 분리(#161)

---
+++ Presentation/Sources/Video/VideoFeedViewController+Delegate.swift
@@ -0,0 +1,33 @@
+//
+//  VideoFeedViewController+Delegate.swift
+//  Presentation
+//
+//  Created by 한지석 on 10/31/24.
+//  Copyright © 2024 com.recordy. All rights reserved.
+//
+
+import UIKit
+
+import Core
+
+@available(iOS 16.0, *)
+extension VideoFeedViewController: FeedWatchDelegate {
+  func play(feed: Feed) {
+    viewModel.postIsFeedWatched(feed: feed)
+  }
+}
+
+@available(iOS 16.0, *)
+extension VideoFeedViewController: ReportWithCopyLinkDelegate {
+  func didTapReport() {
+    updateSheetHeight(Sheet.expandedHeight)
+  }
+
+  func copy() {
+    updateSheetHeight(Sheet.defaultHeight)
+  }
+
+  func cancel() {
+    updateSheetHeight(Sheet.defaultHeight)
+  }
+}
