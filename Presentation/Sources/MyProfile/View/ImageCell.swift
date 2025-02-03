40befbef3b4d784602ab22c280076e14eab77e3c
0gonge <Challengef0802@gmail.com>
2025-02-03 17:35:33 +0900

    feat: ImageCell 구현(#177)

--- Presentation/Sources/MyProfile/View/ImageCell.swift
+++ Presentation/Sources/MyProfile/View/ImageCell.swift
@@ -6,13 +6,93 @@
 //  Copyright © 2025 com. All rights reserved.
 //
 
-import UIKit
+import Common
 import Photos
+import UIKit
+
 
 import SnapKit
 import Then
 
 final class ImageCell: UICollectionViewCell {
   static let identifier = "ImageCell"
-
+  
+  private let imageView = UIImageView()
+  private let selectionIndicator = UIView()
+  private let checkmark = UIImageView()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setUI()
+    setStyle()
+    setLayout()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setUI() {
+    contentView.addSubviews(
+      imageView,
+      selectionIndicator
+    )
+    selectionIndicator.addSubview(checkmark)
+  }
+  
+  private func setStyle() {
+    imageView.do {
+      $0.contentMode = .scaleAspectFill
+      $0.clipsToBounds = true
+    }
+    selectionIndicator.do {
+      $0.layer.borderWidth = 1
+      $0.layer.cornerRadius = 16/2
+      $0.layer.borderColor = CommonAsset.viskitGray01.color.cgColor
+      $0.backgroundColor = CommonAsset.viskitBlack30.color
+    }
+    checkmark.do {
+      $0.image = CommonAsset.selctionCheck.image
+      $0.isHidden = true
+      $0.layer.cornerRadius = 16/2
+    }
+  }
+  
+  private func setLayout() {
+    imageView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+    
+    selectionIndicator.snp.makeConstraints {
+      $0.top.equalTo(imageView.snp.top).offset(6)
+      $0.trailing.equalTo(imageView.snp.trailing).inset(6)
+      $0.width.height.equalTo(16)
+    }
+    
+    checkmark.snp.makeConstraints {
+      $0.edges.equalTo(selectionIndicator)
+      $0.width.height.equalTo(16)
+    }
+  }
+  
+  func configure(with asset: PHAsset) {
+    let imageManager = PHImageManager.default()
+    let options = PHImageRequestOptions()
+    options.deliveryMode = .fastFormat
+    
+    imageManager.requestImage(
+      for: asset,
+      targetSize: bounds.size,
+      contentMode: .aspectFill,
+      options: options
+    ) { image, _ in
+      self.imageView.image = image
+    }
+  }
+  
+  func setSelected(selected: Bool) {
+    selectionIndicator.layer.borderColor = isSelected ? CommonAsset.viskitKakaoYellow.color.cgColor : UIColor.clear.cgColor
+    checkmark.isHidden = !selected
+  }
+  
 }
